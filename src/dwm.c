4c12ab7d98f5ccad0418277d7a3063505876190c
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-12-14 17:00:41 +0200

    formatting

--- src/dwm.c
+++ src/dwm.c
@@ -16,15 +16,15 @@
 #include <X11/Xft/Xft.h>
 
 /* macros */
-#define MAX(A, B)	((A) > (B) ? (A) : (B))
-#define MIN(A, B)	((A) < (B) ? (A) : (B))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define CLEANMASK(mask) (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
 #define INTERSECT(x,y,w,h,m) (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
                              * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
-#define ISVISIBLE(C)	((C->tags & C->mon->tagset[C->mon->seltags]))
-#define LENGTH(X)	(sizeof X / sizeof X[0])
+#define ISVISIBLE(C) ((C->tags & C->mon->tagset[C->mon->seltags]))
+#define LENGTH(X) (sizeof X / sizeof X[0])
 #define WIDTH(X) ((X)->w + 2 * (X)->bw)
-#define HEIGHT(X)	((X)->h + 2 * (X)->bw)
+#define HEIGHT(X) ((X)->h + 2 * (X)->bw)
 #define TAGMASK ((1 << workspaces) - 1)
 
 /* util */
